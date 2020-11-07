1b7b446bb737a7ffebd5084d1cb821601234db64
AlexSavchuk95 <savchukalex57@gmail.com>
2020-11-07 12:15:29 +0200

    Updates

---
+++ precache-manifest.d37e092a9c3f478f2ad80eca28356309.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "820c954dc688e46b72002bdbb41d78e3",
+    "url": "/Starlader/index.html"
+  },
+  {
+    "revision": "f3a641cfb4dc2b6ace21",
+    "url": "/Starlader/static/css/2.06b7ea63.chunk.css"
+  },
+  {
+    "revision": "02be5aa876b6a4d81e64",
+    "url": "/Starlader/static/css/main.9208e151.chunk.css"
+  },
+  {
+    "revision": "f3a641cfb4dc2b6ace21",
+    "url": "/Starlader/static/js/2.a03fd3b3.chunk.js"
+  },
+  {
+    "revision": "02be5aa876b6a4d81e64",
+    "url": "/Starlader/static/js/main.d6e53300.chunk.js"
+  },
+  {
+    "revision": "711238f1d040f6f0ad60",
+    "url": "/Starlader/static/js/runtime-main.2b43a881.js"
+  },
+  {
+    "revision": "cf58c42ffb182b16c98f46467fdd8f25",
+    "url": "/Starlader/static/media/bg.cf58c42f.png"
+  },
+  {
+    "revision": "fe7e96d2be946110a8f137eb78a875ba",
+    "url": "/Starlader/static/media/logoB.fe7e96d2.png"
+  },
+  {
+    "revision": "d4875dd4ec4cf40991c51e9f2a860e91",
+    "url": "/Starlader/static/media/star_nA.d4875dd4.png"
+  }
+]);
